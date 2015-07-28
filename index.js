78e03d1cf7f722b2da9d2411d5c94ca3c693dfec
Tatu Tamminen <ttamminen@gmail.com>
2015-07-28 17:50:13 +0300

    changes indentation

--- index.js
+++ index.js
@@ -8,36 +8,36 @@ var connectionConf = JSON.parse(fs.readFileSync('conf.json', 'utf8'));
 var collectionNameToCreate = 'my-collection';
 
 createCollection(connectionConf, collectionNameToCreate)
-	.spread(function (response, body) {
-		if(response.statusCode !== 201) {
-			console.log('Could not create database');
-			console.log(response.statusCode);
-			return;
-		}
+  .spread(function (response, body) {
+    if(response.statusCode !== 201) {
+      console.log('Could not create database');
+      console.log(response.statusCode);
+      return;
+    }
 
-		console.log('Database created');
-	})
-	.error(function (err) {
-		console.log(err);
-	});
+    console.log('Database created');
+  })
+  .error(function (err) {
+    console.log(err);
+  });
 
 function createCollection(conf, name) {
-	var baseUrl = 'https://api.compose.io/';
-	var createCollectionUrl = 'deployments/' + 
-													conf.accountName + '/' + 
-													conf.deploymentName + '/mongodb/' + 
-													conf.databaseName + '/collections';
+  var baseUrl = 'https://api.compose.io/';
+  var createCollectionUrl = 'deployments/' + 
+                          conf.accountName + '/' + 
+                          conf.deploymentName + '/mongodb/' + 
+                          conf.databaseName + '/collections';
 
-	return request.postAsync({
-		baseUrl: baseUrl,
-		url: createCollectionUrl,
-		headers: {
-			'Authorization': 'Bearer ' + conf.token,
-			'Accept-Version': conf.apiVersion
-		},
-		json: true,
-		body: {
-			name: name
-		}
-	});
+  return request.postAsync({
+    baseUrl: baseUrl,
+    url: createCollectionUrl,
+    headers: {
+      'Authorization': 'Bearer ' + conf.token,
+      'Accept-Version': conf.apiVersion
+    },
+    json: true,
+    body: {
+      name: name
+    }
+  });
 }
